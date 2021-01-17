f9cc86489fd61488d7fdce0cb7d75e86915ef3a3
Harsha Kadekar <harsha.kadekar@gmail.com>
2021-01-17 15:21:45 -0800

    Common Interface for Lexer

--- src/main/java/org/plp/isa/AsmLexer.java
+++ src/main/java/org/plp/isa/AsmLexer.java
@@ -1,2 +1,17 @@
-package org.plp.isa;public class AsmLexer {
+package org.plp.isa;
+
+import lombok.NonNull;
+
+import java.util.List;
+
+/**
+ * Given a string it will provide the list of tokens present in that string
+ */
+public interface AsmLexer {
+    /**
+     * Given a string it will provide a list of tokens consisting of the string
+     * @param input String which needs to be broken into individual tokens
+     * @return List of {@link AsmToken} consisting of that string
+     */
+    List<AsmToken> lex(@NonNull String input);
 }
